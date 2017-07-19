28f4e8fbc8be01c1160663aab0d7cbfbd4fb97aa
KD8BXP <kd8bxp@aol.com>
2017-07-19 19:09:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Livera Robot Code Demo
+
+This is for the new HiCat-Livera board, which is a combination of Arduino Leonardo, Linux, MPU6050, WIFI AP/AD, 720p HD camera, and Mobile Apps (Web App)
+This is a slightly modified version of the demo code for the Livera Robot
+ changed AIN4 from pin 7 to pin 8, added 20 to left and right for
+ increased PWM needed for my bigger motors
+
+## Installation
+
+Follow documention here:
+http://hicat.io/Livera-Documentation/#quick-start-manual
+
+Library Needed:
+https://github.com/hicat-tech/Arduino-SerialCommand
+https://github.com/hicat-tech/hicat_arduino_library
+
+## Things To Do
+
+## Usage
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Credits
+
+Copyright (c) 2017 LeRoy Miller
+
+## License
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses>
---
+++ hicat_livera_bitty_bot_convert_robot_code/hicat_livera_bitty_bot_convert_robot_code.ino
@@ -0,0 +1,164 @@
+// hicat robot demo
+// get SerialCommand library from https://github.com/hicat-tech/Arduino-SerialCommand
+// Slightly Modified by LeRoy Miller July 17, 2017
+// changed AIN4 from pin 7 to pin 8, added 20 to left and right for
+// increased PWM needed for my bigger motors
+
+#include <Servo.h>
+#include <SerialCommand.h>
+
+int PWM1 = 9;     // control servo
+int PWM_CE = 11;  // servo enable
+
+int AIN1 = 4;  // control motor1
+int AIN2 = 5;  // control motor1 pwm
+int AIN3 = 6;  // control motor2 pwm
+int AIN4 = 8;  // control motor2
+
+int BIN3 = A2;  //control laser
+int BIN4 = A3;  //control laser
+
+Servo myservo;
+SerialCommand sCmd;
+
+uint8_t camera_angle = 120;
+uint8_t camera_angle_range[] = {
+  60, 150
+};
+uint8_t laser_is_on = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+//  while (!Serial);
+  Serial.println("----- Car ----");
+  
+  // initialize the digital pin as an output.
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN3, OUTPUT);
+  pinMode(AIN4, OUTPUT);
+  pinMode(BIN3, OUTPUT);
+  pinMode(BIN4, OUTPUT);
+  digitalWrite(BIN3, LOW);  // turn off LASER
+  digitalWrite(BIN4, LOW);
+
+  pinMode(PWM_CE, OUTPUT);
+  myservo.attach(PWM1);
+  digitalWrite(PWM_CE, HIGH);
+  myservo.write(camera_angle);
+  digitalWrite(PWM_CE, LOW); //disable servo
+
+
+  // Setup callbacks for SerialCommand commands
+  sCmd.addCommand("laser", trigger_laser);
+  sCmd.addCommand("camera", adjust_camera);
+  sCmd.addCommand("motor", set_motor);
+  sCmd.setDefaultHandler(unrecognized);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  sCmd.readSerial((Stream&)Serial1);
+}
+
+void trigger_laser()
+{
+  if (laser_is_on) {
+    digitalWrite(BIN3, LOW);  // turn off LASER
+    laser_is_on = 0;
+    Serial.println("laser off");
+  }
+  else {
+    digitalWrite(BIN3, HIGH);  // turn on LASER
+    laser_is_on = 1;
+    Serial.println("laser on");
+  }
+}
+
+void adjust_camera()
+{
+  char *param;
+
+  param = sCmd.next();
+  if (!strcmp(param, "down")) {
+    if (camera_angle < camera_angle_range[1]) {
+      camera_angle += 10;
+
+      digitalWrite(PWM_CE, HIGH);
+      myservo.write(camera_angle);
+      digitalWrite(PWM_CE, HIGH);
+    }
+  }
+  else if (!strcmp(param, "up")) {
+    if (camera_angle > camera_angle_range[0]) {
+      camera_angle -= 10;
+
+      digitalWrite(PWM_CE, HIGH);
+      myservo.write(camera_angle);
+      digitalWrite(PWM_CE, HIGH);
+    }
+  }
+
+  Serial.print("camera angle: ");
+  Serial.println(camera_angle);
+}
+
+void set_motor()
+{
+  char *param;
+  int16_t left = 0;
+  int16_t right = 0;
+
+  param = sCmd.next();
+  if (param == NULL) {
+    return;
+  }
+  left = atoi(param) + 20;  // added +20 for my wheels
+
+  param = sCmd.next();
+  if (param == NULL) {
+    return;
+  }
+  right = atoi(param) + 20; // added + 20 for my wheels
+
+  Serial.print("motor ");
+  Serial.print(left);
+  Serial.print(" ");
+  Serial.println(right);
+
+
+  if (left > 30) {
+    analogWrite(AIN2, left);
+    digitalWrite(AIN1, LOW);
+  } else if (left < -30)  {
+    analogWrite(AIN2, 255 + left);
+    digitalWrite(AIN1, HIGH);
+  }
+  else {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+  }
+
+  if (right > 30) {
+    analogWrite(AIN3, right);
+    digitalWrite(AIN4, LOW);
+  }
+  else if (right < -30) {
+    analogWrite(AIN3, 255 + right);
+    digitalWrite(AIN4, HIGH);
+  }
+  else {
+    digitalWrite(AIN3, LOW);
+    digitalWrite(AIN4, LOW);
+  }
+
+}
+
+
+// This gets set as the default handler, and gets called when no other command matches.
+void unrecognized(const char *command) {
+  Serial.println('?');
+}
+
